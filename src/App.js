379c0e0cca3f9766b493e068de79c6d701f581b5
alex <barmaleyalex@gmail.com>
2020-08-31 13:20:15 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import NewsCards from './components/NewsCards/NewsCards';
 import './App.css';
 
 function App() {
+  const [newsArticles,setNewsArticles]= useState([]);
+
+
+
+
+  useEffect(()=>{
+    alanBtn({
+      key:process.env.REACT_APP_ALAN_API_KEY,
+      onCommand:({command,articles})=>{
+        if(command === 'newHeadlines'){
+          setNewsArticles(articles);
+        }
+      }
+    })
+  },[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>ALAN</h1>
+     <NewsCards articles={newsArticles}/>
     </div>
   );
 }
---
+++ src/components/NewsCard/styles.js
@@ -0,0 +1,8 @@
+import  {makeStyles} from '@material-ui/core/styles';
+
+const styles = makeStyles({
+    media:{
+        height:250,
+    }
+});
+export default styles;
