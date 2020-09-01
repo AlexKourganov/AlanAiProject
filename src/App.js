2c33f1e99cae4865ce9cc80f7d045d9e43197f01
alex <barmaleyalex@gmail.com>
2020-09-01 13:17:49 -0700

    finished alan ai

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React,{useState,useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
-
+import useStyles from './styles';
+import wordsToNumbers from 'words-to-numbers';
 import NewsCards from './components/NewsCards/NewsCards';
 import './App.css';
 
 function App() {
+  const classes =  useStyles();
   const [newsArticles,setNewsArticles]= useState([]);
+  const [activeArticle,setActiveArticle]= useState(-1);
 
 
 
@@ -13,9 +16,26 @@ function App() {
   useEffect(()=>{
     alanBtn({
       key:process.env.REACT_APP_ALAN_API_KEY,
-      onCommand:({command,articles})=>{
+      onCommand:({command,articles,number})=>{
         if(command === 'newHeadlines'){
           setNewsArticles(articles);
+          setActiveArticle(-1);
+        }else if(command === 'highlight'){
+          setActiveArticle((prevActiveArticle)=>prevActiveArticle + 1);
+        }else if(command === 'open'){
+          
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number,{fuzzy:true}) : number;
+          console.log(parsedNumber);
+          const article  = articles[parsedNumber-1];
+
+          if(parsedNumber > 20){
+            alanBtn().playText('Please try that again.');
+          }else if(article){
+            window.open(article.url,'_blank');
+            alanBtn().playText('Opening.....')
+          }
+
+          
         }
       }
     })
@@ -24,8 +44,10 @@ function App() {
 
   return (
     <div className="App">
-     <h1>ALAN</h1>
-     <NewsCards articles={newsArticles}/>
+     <div className={classes.logoContainer}>
+       <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo"/>
+     </div>
+     <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
     </div>
   );
 }
